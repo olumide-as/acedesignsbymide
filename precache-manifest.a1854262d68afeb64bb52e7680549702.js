544d2496f69741fb4680e0d3b1b18a9d6f347cba
olumide-as <adigunsamuel84@gmail.com>
2022-08-11 09:46:25 +0100

    Initial commit

---
+++ precache-manifest.a1854262d68afeb64bb52e7680549702.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b007304a2a2a49c565",
+    "url": "/static/css/main.91e43a4a.chunk.css"
+  },
+  {
+    "revision": "b8b007304a2a2a49c565",
+    "url": "/static/js/main.35a943b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a6bbc082662a9253900",
+    "url": "/static/css/2.0dee9433.chunk.css"
+  },
+  {
+    "revision": "5a6bbc082662a9253900",
+    "url": "/static/js/2.ada73975.chunk.js"
+  },
+  {
+    "revision": "da6e59cf08c1512a87b7febb5f78e55e",
+    "url": "/static/media/Masking.da6e59cf.png"
+  },
+  {
+    "revision": "cf0918a36f01aab9696c9d5e683a2288",
+    "url": "/static/media/ace-light.cf0918a3.svg"
+  },
+  {
+    "revision": "5ef7b5e783cfe969ceada96b50921a9f",
+    "url": "/static/media/Lonely Popx.5ef7b5e7.png"
+  },
+  {
+    "revision": "85f9f946883c7d01a4a60609fc25d8c9",
+    "url": "/index.html"
+  }
+];
